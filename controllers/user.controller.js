db1b943fd5fc25ef53fff4977995fe9ec2b7a1ba
Vo Nhat Trieu <vntrieu25879@gmail.com>
2021-05-07 12:05:52 +0700

    modofy dir to upload image when sign up

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,6 +1,7 @@
 const md5 = require('md5');
 const User = require('../models/user.model');
 const jwt = require('jsonwebtoken');
+const fs = require('fs');
 
 module.exports = {
     login: async(req, res) => {
@@ -9,29 +10,30 @@ module.exports = {
         await User.find({username: username, password: password})
         .then(user => {
             if(user.length > 0){
-                const data = {...user[0]['_doc']};
-                delete data.password;
+                const userInformation = {...user[0]['_doc']};
+                delete userInformation.password;
                 const token = jwt.sign({
-                    userId: data['_id']
+                    userId: userInformation['_id']
                 }, process.env.SECRET_KEY);
                 res.json({
-                    data,
+                    userInformation,
                     token,
                 });
             }else{
                 res.json('User not found!');
             }
         })
+
     },
     loginByToken: async (req, res) => {
         const userId = req.user.userId;
         await User.findById(userId)
         .then(user => {
-            if(student){
-                const data = {...user['_doc']};
-                delete data.password;
+            if(user){
+                const userInformation = {...user['_doc']};
+                delete userInformation.password;
                 res.json({
-                    data,
+                    userInformation
                 });
             }else{
                 res.json('Token is wrong!');
@@ -70,8 +72,14 @@ module.exports = {
         const data = {...req.body, password: md5(req.body.password)};
         const image = req.files.image;
         const tailPath = image.mimetype.substring(6);
-        const path = process.env.DIR_IMAGE + data.code + '.' + tailPath;
-        data.img = path;
+        const dir = process.env.DIR_IMAGE + data.code;
+        if (!fs.existsSync(dir)){
+            fs.mkdirSync(dir);
+        }
+        let countFile = 0;
+        fs.readdir(dir, (err, files) => countFile = files.length + 1);
+        const path = dir + '/' + countFile + '.' + tailPath;
+        data.img = './img/' + data.code + '/' + countFile + '.' + tailPath;
         const newUser = new  User(data);
         await User.create(newUser)
         .then(() => res.json("You have signed up successfully!"))
